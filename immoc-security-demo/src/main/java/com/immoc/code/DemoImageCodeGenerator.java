2d1579b03bcef1b1822b92565d80e4f97ce23e24
yongjianz <296654170@qq.com>
2020-01-08 20:04:05 +0800

    4-8,重构图形验证器，可以配置化，可以自定义图形认证码

--- immoc-security-demo/src/main/java/com/immoc/code/DemoImageCodeGenerator.java
+++ immoc-security-demo/src/main/java/com/immoc/code/DemoImageCodeGenerator.java
@@ -7,6 +7,7 @@ import org.springframework.web.context.request.ServletWebRequest;
 
 @Component("imageCodeGenerator")
 public class DemoImageCodeGenerator implements ValidateCodeGenerator {
+
     @Override
     public ImageCode generate(ServletWebRequest request) {
         System.out.println("更高级的图形验证码生成代码");
