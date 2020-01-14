b7fb3e6415984cab28b3039b27b6749878cdea40
yongjianz <296654170@qq.com>
2020-01-14 21:47:53 +0800

    4-12 开发短信验证码接口

--- immoc-security-browser/src/main/java/com/immoc/security/browser/BrowsercurityConfig.java
+++ immoc-security-browser/src/main/java/com/immoc/security/browser/BrowsercurityConfig.java
@@ -2,7 +2,9 @@ package com.immoc.security.browser;
 
 import com.immoc.security.browser.authentication.MyAuthenticationFailureHandler;
 import com.immoc.security.browser.authentication.MyAuthenticationSuccessHandler;
+import com.immoc.sercurity.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.immoc.sercurity.core.properties.SecurityProperties;
+import com.immoc.sercurity.core.validate.code.SmsCodeFilter;
 import com.immoc.sercurity.core.validate.code.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +28,8 @@ public class BrowsercurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyAuthenticationFailureHandler failHandlerHandler;
 
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
 
 
     @Bean
@@ -36,10 +40,17 @@ public class BrowsercurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-         ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-         validateCodeFilter.setFailureHandler(failHandlerHandler);
-         validateCodeFilter.setSecurityProperties(securityProperties);
-         validateCodeFilter.afterPropertiesSet();
+
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setFailureHandler(failHandlerHandler);
+        validateCodeFilter.setSecurityProperties(securityProperties);
+        validateCodeFilter.afterPropertiesSet();
+
+        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
+        smsCodeFilter.setFailureHandler(failHandlerHandler);
+
+        smsCodeFilter.setSecurityProperties(securityProperties);
+        smsCodeFilter.afterPropertiesSet();
             http.formLogin()//设置为表单登录
                 .loginPage("/authentication/require")//设置登录页面
                 .loginProcessingUrl("/authentication/form")//修改UsernamePasswordAuthenticationFilter默认处理的登录表单提交url
@@ -47,11 +58,14 @@ public class BrowsercurityConfig extends WebSecurityConfigurerAdapter {
                 .failureHandler(failHandlerHandler)
                 .and() //所有请求都需要登录认证
                 .authorizeRequests()
-                .antMatchers("/code/image","/code/sms","/authentication/require",securityProperties.getBrowser().getLoginPage()).permitAll()//配置的页面无需认证
+                .antMatchers("/authentication/mobile","/code/image","/code/sms","/authentication/require",securityProperties.getBrowser().getLoginPage()).permitAll()//配置的页面无需认证
                 .anyRequest()
                 .authenticated()
                 .and()
                 .csrf().disable()
-                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .apply(smsCodeAuthenticationSecurityConfig);
     }
 }
+
---
+++ immoc-security-core/src/main/java/com/immoc/sercurity/core/validate/code/SmsCodeFilter.java
@@ -0,0 +1,107 @@
+package com.immoc.sercurity.core.validate.code;
+
+import com.immoc.sercurity.core.properties.SecurityProperties;
+import com.immoc.sercurity.core.validate.code.image.ImageCode;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SmsCodeFilter extends OncePerRequestFilter implements InitializingBean {
+
+
+    private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
+
+    private Set<String> urls = new HashSet<>();
+
+    private AuthenticationFailureHandler failureHandler;
+
+    private SecurityProperties securityProperties;
+
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public void afterPropertiesSet() throws ServletException {
+        super.afterPropertiesSet();
+        String [] configUrls = StringUtils.splitByWholeSeparatorPreserveAllTokens(securityProperties.getCode().getSms().getUrl(),",");
+        for(String configUrl : configUrls){
+            urls.add(configUrl);
+        }
+        urls.add("/authentication/mobile");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        boolean action = false;
+        for(String url : urls){
+            if(antPathMatcher.match(url, request.getRequestURI())){
+                action = true;
+            }
+        }
+
+        if(action){
+            try{
+                validate(new ServletWebRequest(request));
+            }catch (ValidateCodeException e){
+                failureHandler.onAuthenticationFailure(request, response ,e);
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void validate(ServletWebRequest request) throws ServletRequestBindingException {
+        ValidateCode codeInSession = (ValidateCode) sessionStrategy.getAttribute(request,ValidateCodeProcessor.SESSION_KEY_PREFIX+"SMS");
+
+        String codeInRequest = ServletRequestUtils.getStringParameter(request.getRequest(),"smsCode");
+
+        if(StringUtils.isBlank(codeInRequest)){
+            throw new ValidateCodeException("验证码不能为空");
+        }
+
+        if(codeInSession==null){
+            throw new ValidateCodeException("验证码不存在");
+        }
+
+        if (codeInSession.isExpired()){
+            sessionStrategy.removeAttribute(request, ValidateCodeProcessor.SESSION_KEY_PREFIX+"SMS");
+            throw new ValidateCodeException("验证码已过期");
+        }
+
+        if(!StringUtils.equals(codeInSession.getCode(), codeInRequest)){
+            throw new ValidateCodeException("验证码不匹配");
+        }
+
+        sessionStrategy.removeAttribute(request, ValidateCodeProcessor.SESSION_KEY_PREFIX+"SMS");
+    }
+
+    public SecurityProperties getSecurityProperties() {
+        return securityProperties;
+    }
+
+    public void setSecurityProperties(SecurityProperties securityProperties) {
+        this.securityProperties = securityProperties;
+    }
+
+    public AuthenticationFailureHandler getFailureHandler() {
+        return failureHandler;
+    }
+
+    public void setFailureHandler(AuthenticationFailureHandler failureHandler) {
+        this.failureHandler = failureHandler;
+    }
+}
